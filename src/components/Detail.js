c78148d26dadea445ccd7a5f9eb704c1c98ef689
Sharooq Salaudeen <coolsharooqs@gmail.com>
2021-06-10 14:08:53 +0530

    Added bug fix in Recommends, NewDisney, Trending, Originals

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
 function Detail() {
-  return <div>Detail</div>;
+  return <Container>Detail</Container>;
 }
 
 export default Detail;
+
+const Container = styled.div`
+  position: relative;
+  min-height: calc(100vh-250px);
+  overflow-x: hidden;
+  display: block;
+  top: 73px;
+  padding: 0 calc(3.5vw + 5px);
+`;
